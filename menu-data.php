491f7a6ee6c105289d4ca73fcf4b8298dd093661
Jeremy Estes <estesjeremy96@gmail.com>
2025-07-28 12:37:59 -0400

    Full menu (sans Catering)

---
+++ menu-data.php
@@ -0,0 +1,133 @@
+<?php
+    $menu = [
+        "Angelo's Special(s)" => [
+            'THE JERSEY GIRL: Grilled Corn, Fresh Jersey Blueberries, and Chicken Cutlet tossed in a Blueberry Vinaigrette Over Romaine Lettuce'
+        ],
+        'Starters N Wings' => [
+            'Fries' => [
+                '<b>Plain Fries</b> - $5',
+                '<b>Cheese Fries</b> w/ Choice of Whiz, Melted American, Mozzarella, Sharp prov - $7',
+                '<b>Pizza Fries</b> w/ Melted American, in Angie\'s Gravy - $7',
+                '<b>Cheesesteak Fries</b> w/ Rib Eye Steak, Whiz or Melted American - $12',
+                '<b>Shrimp Fries</b> w/ Fried Shrimp w/ Choice of Sauce and Melted American - $16',
+                '<b>Buffalo Chicken Cheesesteak Fries</b> w/ Buffalo Sauce and Melted American - $12'
+            ],
+            'Other Starters' => [
+                '<b>Fried Shrimp</b> in our Signature Buffalo Sauce or Sweet-n-Sexy Sauce - $14',
+                '<b>Mozzarella Sticks</b> - $8',
+                '<b>Onion Rings</b> - $8',
+                '<b>Fried Pickles</b> w/ Chipotle Ranch - $8',
+                '<b>Eggplant Stack</b> w/ Eggplant, Burrata Cheese, Mario\'s Tomato Salad Topped w/ Balsamic Glaze - $12',
+                '<b>Individual Meatballs</b> - $3.50'
+            ],
+            'wings' => '<b>10 Bone-in or Boneless</b> - $14<br><span class="italic">Sauces: </span>Signature Buffalo Sauce, Sweet-n-Sexy (hot, honey, garlic, bbq), Barbeque, Garlic Parm, Dry Rub, Buffalo Garlic Parm'
+        ],
+        'Sandwiches' => [
+            'Meatball - $16' => [
+                '<b>Signature Meatball</b> in Angie’s Gravy',
+                '<b>Fried Meatball or Sausage</b> w/ Cheese',
+                '<b>Fried Meatball or Sausage Parm</b> w/ Angie’s Gravy and Melted American',
+                '<b>Fried Meatball or Sausage Maria</b> w/ Broccoli Rabe, Roasted Reds, and Melted American',
+                '<b>Fried Meatball or Sausage Italian</b> w/ Sharp Prov and Broccoli Rabe',
+                '<b>Fried Meatball Classic</b> w/ Mario’s Tomato Salad and Melted American',
+                '<b>Fried Meatball Hoagie</b> w/ Mayo, Lettuce, Mario’s Tomato Salad and Melted American',
+                '<b>Fried Meatball Slammer</b> w/ Mario’s Tomato Salad, Banana Peppers and Melted American',
+                '<b>Fried Meatball Banger</b> w/ Mario’s Tomato Salad, Broccoli Rabe and Melted American',
+                '<b>Buffalo Fried Meatball</b> w/ Our Signature Buffalo Sauce and Melted American',
+                '<b>Fried Meatball or Sausage</b> w/ Peppers, Onions and Melted American',
+                '<b>Individual Meatballs</b> - $3.50'
+            ],
+            'Chicken Cutlet - $14' => [
+                '<b>Cutlet</b> w/ Choice of Cheese',
+                '<b>Cutlet Parm</b> w/ Angie’s Gravy and Melted American',
+                '<b>Cutlet Classic</b> w/ Mario’s Tomato Salad and Melted American',
+                '<b>Cutlet Italian</b> w/ Sharp Prov and Broccoli Rabe',
+                '<b>Cutlet Hoagie</b> w/ Mayo, Lettuce, Mario’s Tomato Salad and Melted American',
+                '<b>Buffalo Cutlet</b> w/ Our Signature Buffalo Sauce and Melted American',
+                '<b>Cutlet Banger</b> w/ Broccoli Rabe, Mario’s Tomato Salad and Melted American',
+                '<b>Cutlet Slammer</b> w/ Mario’s Tomato Salad, Banana Peppers and Melted American',
+                '<b>Buffalo Cutlet Slammer</b> w/ Our Signature Buffalo Sauce, Mario’s Tomato Salad, Banana Peppers and Melted American',
+                '<b>Sweet n Sexy Cutlet</b> w/ Our Sweet-n-Sexy Sauce and Melted American',
+                '<b>Cutlet Maria</b> w/ Roasted Reds, Broccoli Rabe and Melted American',
+                '<b>Chopped Caesar</b> w/ Chopped Cutlet, Romaine tossed in Caesar Dressing Served on Toasted Garlic Roll'
+            ],
+            'Specialty Cutlets - $16' => [
+                '<b>Chipotle Ranch</b> w/ Chipotle Ranch, Bacon, Mario\'s Tomato Salad and Cooper Sharp',
+                '<b>Vodka</b> w/ Vodka Sauce, Burrata and Pesto',
+                '<b>Waffle</b> w/ Garlic Maple Glaze, Cooper Sharp on a Belgium Waffle',
+                '<b>The Mac</b> w/ Choice of Sauce Topped w/ Mac-n-Cheese',
+                '<b>Northeast Cuban</b> w/ Pickles, Yellow Mustard, Pulled Pork and Cooper Sharp',
+                '<b>CBR</b> w/ Bacon, Ranch and Caramelized Onions'
+            ],
+            'Steak (Rib Eye) - $14' => [
+                '<b>Plain</b>',
+                '<b>Cheese Steak</b> w/ Choice of Melted American, Whiz, Sharp Provolone or Mozzarella',
+                '<b>Pizza Steak</b> w/ Angie\'s Gravy and Melted American',
+                '<b>Pepperoni Pizza Steak</b> w/ Angie\'s Gravy, Melted American and Pepperoni'
+            ],
+            'Chicken Cheesesteak - $14' => [
+                '<b>Plain</b>',
+                '<b>Chicken Cheesesteak</b> w/ Choice of Melted American, Whiz, Sharp Provolone or Mozzarella',
+                '<b>Buffalo Chicken Cheesesteak</b> w/ Our Signature Buffalo Sauce and Choice of Cheese',
+                '<b>Sweet-n-Sexy Chicken Steak</b> w/ Sweet-n-Sexy Sauce and Choice of Cheese',
+                '<b>Pizza Chicken Cheesesteak</b> w/ Angie\'s Gravy and Melted American'
+            ],
+            'Roast Pork - $14' => [
+                '<b>Roast Pork</b>',
+                '<b>Roast Pork</b> w/ Choice of Sharp Provolone, Whiz, Melted American or Mozzarella',
+                '<b>Roast Pork Italian</b> w/ Sharp Provolone and Broccoli Rabe',
+                '<b>Roast Pork</b> w/ Roasted Red Peppers and Melted American'
+            ],
+            'Eggplant - $14' => [
+                '<b>Eggplant Parm</b> w/ Angie’s Gravy and Melted American',
+                '<b>Eggplant Italian</b> w/ Sharp Prov, Broccoli Rabe or Spinach',
+                '<b>Eggplant</b> w/ Mario’s Tomato Salad, Roasted Reds, Banana Peppers, and Spinach or Broccoli Rabe'
+            ]
+        ],
+        'Pizza/Stromboli' => [
+            'Pizza' => [
+                '<b>Plain</b> w/Cheese and Sauce - $15',
+                '<b>White w/Garlic</b>, Olive Oil, and Parm - $15' 
+            ],
+            'Specialty Pizzas' => [
+                '<b>Signature</b> - White w/Spinach and Mario\'s Tomato Salad - $22',
+                '<b>The Looch</b> - White w/Fried Meatball, Spinach and Mario’s Tomato Salad - $25',
+                '<b>The Angie</b> - Topped with Meatballs and Melted American - $25',
+                '<b>The Mia</b> - Buffalo Cutlet w/Our Signature Buffalo Sauce and Melted American - $22',
+                '<b>The Rocco</b> - Rib Eye Cheesesteak, Sauce and Melted American - $24',
+                '<b>The Mario</b> - White w/Sausage and Broccoli Rabe - $24',
+                '<b>The Francesca</b> - Cutlet Parm w/Sauce, Mozzarella and Melted American - $22',
+                '<b>The David</b> - White w/Mario\'s Tomato Salad - $20',
+                '<b>The Frankie-n-Marie</b> - w/Fried Meatball, Sausage, Pepperoni, Mozzarella and Sauce - $25',
+                '<b>The Chick</b> - White w/Cutlet in Our Sweet-n-Sexy Sauce, Mozzarella and Melted American - $22',
+                '<b>The Elana</b> - White w/Mario’s Tomato Salad, Banana Peppers and American Cheese - $22',
+                '<b>The Mila</b> - Buffalo Fried Meatball w/Our Signature Buffalo Sauce, Mozzarella and Melted American - $25',
+                '<b>The Isabella</b> - Rib Eye Cheesesteak w/Mushrooms, Fried Onions and Melted American - $26'
+            ],
+            'Stromboli' => [
+                '<b>Plain</b> w/Choice of Cheese - $15',
+                '<b>Buffalo Chicken</b> w/Chicken Cheesesteak in Our Signature Buffalo Sauce and Melted American - $22',
+                '<b>Cheesesteak</b> w/Rib Eye and Melted American - $22',
+                '<b>Sweet-n-Sexy Chicken</b> w/Chicken Cheesesteak in Our Sweet-n-Sexy Sauce and Melted American - $22',
+                '<b>Sausage-n-Rabe</b> w/Sausage, Broccoli Rabe and Melted American served w/side of Mario\'s Tomato Salad - $24',
+                '<b>Spinach-n-Cheese</b> w/Sauteed Spinach, Mozzarella and Melted American w/Side of Mario\'s Tomato Salad - $22',
+                '<b>Sanjo</b> w/Cutlet, Roasted Reds and Spinach - $25'
+            ]
+
+        ],
+        'Pasta (w/ Garlic Bread)' => [
+            '<b>Penne in Angie’s Gravy</b> - $10',
+            '<b>Penne w/Meatballs in Angie\'s Gravy</b> - $15',
+            '<b>Penne w/Cutlet in Angie\'s Gravy</b> - $15',
+            '<b>Penne w/Eggplant in Angie’s Gravy</b> - $14',
+            '<b>Penne w/Garlic, Olive Oil and Mario\'s Tomato Salad</b> - $14',
+            '<b>Penne w/Mushrooms, Sauteed Spinach in Garlic and Oil Sauce</b> - $14'
+        ],
+        'Salad' => [
+            '<b>Signature Salad</b> w/Romaine, Onion and Mario’s Tomato Salad - $10',
+            '<b>Buffalo Cutlet Salad</b> - Romaine with Buffalo Cutlet, Mario\'s Tomato Salad, Banana Peppers Topped with Melted American - $14',
+            '<b>Chicken Caesar Salad</b> w/Romaine, Cutlet and Caesar Dressing - $14',
+            '<b>Nina</b> - Caesar Salad Topped with Buffalo Cutlet'
+        ]
+        //,'Catering' => []
+    ];
